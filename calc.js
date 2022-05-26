c42274ccb87b93e14093efc581b8bbb727a8d220
Gavin Stirling <gavinstir@gmail.com>
2022-05-26 16:11:34 +0100

    Git calculator to work

--- calc.js
+++ calc.js
@@ -2,89 +2,139 @@
 // Declaring calculator object
 
 const calculator = {
-    firstNumber: 0,
-    result: '0',
-    waitingForSecondOperator: false,
-    operator: null
-}
+  firstNumber: 0,
+  result: "0",
+  waitingForSecondNumber: false,
+  operator: null,
+  history: [],
+};
 
 const workArea = document.querySelector(".workarea");
 const operatorButtons = document.querySelector(".operators");
-const numberButtons = document.querySelector(".numbers")
-const clearButtons = document.querySelector(".clear")
+const numberButtons = document.querySelector(".numbers");
+const clearButtons = document.querySelector(".clear");
 
-const setDisplay = (() => {
-    workArea.innerText = calculator.result;
-})
+const setDisplay = () => {
+  workArea.innerText = calculator.result;
+};
 
-const clear = (() => {
-    workArea.innerText = "";
-})
-const clearAll = (() => {
-    calculator.result = "0";
-    workArea.innerText = "";
-})
+const clear = () => {
+  const holder = calculator.result;
+  calculator.result = holder.slice(0, -1);
+};
 
-const inputNumber = ((number) => {
-    const numberString = calculator.result;
-    if (numberString === '0'){
-        calculator.result = number;
+const clearAll = () => {
+  calculator.result = "0";
+  calculator.firstNumber = 0;
+  calculator.history = [];
+  workArea.innerText = "";
+  calculator.waitingForSecondNumber = false;
+};
+
+const inputNumber = (number) => {
+  if (calculator.waitingForSecondNumber === true) {
+    calculator.result = number;
+    calculator.waitingForSecondNumber = false;
+  } else {
+    if (calculator.result === "0") {
+      calculator.result = number;
     } else {
-        calculator.result += number;
+      calculator.result += number;
     }
-    
-    
-})
+  }
+};
 
-const handleDecimal =((decimal) => {
+const handleDecimal = (decimal) => {
+  if (calculator.waitingForSecondNumber) {
+    calculator.result = "0.";
+    calculator.waitingForSecondNumber = false;
+    return
+  }
+  
     if (!calculator.result.includes(decimal)) {
-        calculator.result += decimal;
-    }
-})
+    calculator.result += decimal;
+  }
+};
 
-const handleClick = ((event) => {
-    if (!event.target.matches('button')){
-        return;
-    }
-    if (event.target.classList.contains("operators__operator")){
-        console.log("operator ", event.target.value);
+const handleOperator = (operInput) => {
+  const input = parseFloat(calculator.result);
 
-    }
-    if (event.target.classList.contains("numbers__number")){
-        const num = event.target.innerText;
-        console.log("number ", num);
-        inputNumber(num);
-        
-    }
-    if (event.target.classList.contains("clear__clear")){
-        const action = event.target.innerText;
-        console.log("clear ", action);
-        if (action === "Clear"){
-            clear();
-        } else if (action === "Clear All") {
-            clearAll();
-        }
+  if (calculator.firstNumber === 0 && !isNaN(input)) {
+    calculator.firstNumber = input;
+  } else if (calculator.operator) {
+    const result = calculate(
+      calculator.firstNumber,
+      input,
+      calculator.operator
+    );
+    calculator.result = `${result.toFixed(2)}`;
+    calculator.firstNumber = result;
+  }
 
-    }
-    if (event.target.classList.contains("numbers__decimal")){
-        const decimal = event.target.innerText;
-        console.log("decimal ", decimal);
-        handleDecimal(decimal);
-    }
+  calculator.waitingForSecondNumber = true;
+  calculator.operator = operInput;
+};
 
+const calculate = (firstNumber, secondNumber, operator) => {
     
-    setDisplay();
-    console.log(calculator.result);
-    
-})
+  if (operator === "+") {
+    return firstNumber + secondNumber;
+  } else if (operator === "-") {
+    return firstNumber - secondNumber;
+  } else if (operator === "*") {
+    return firstNumber * secondNumber;
+  } else if (operator === "/") {
+    if (secondNumber === 0) {
+      return "Error, cannot divide by zero.";
+    } else {
+      return firstNumber / secondNumber;
+    }
+  } else if (operator === "=") {
+    return firstNumber + secondNumber;
+  }
+};
+
+const handleClick = (event) => {
+  if (!event.target.matches("button")) {
+    return;
+  }
+  if (event.target.classList.contains("operators__operator")) {
+    const operator = event.target.value;
+    if (operator === "="){
+        handleOperator(operator);
+        calculator.operator = null;
+    } else {
+        handleOperator(operator);
+    }
+  }
+  if (event.target.classList.contains("numbers__number")) {
+    const num = event.target.innerText;
+    console.log("number ", num);
+    inputNumber(num);
+  }
+  if (event.target.classList.contains("clear__clear")) {
+    const action = event.target.innerText;
+    console.log("clear ", action);
+    if (action === "Clear") {
+      console.log("clear clear ", action);
+      clear();
+    } else if (action === "Clear All") {
+      console.log("clear all ", action);
+      clearAll();
+    }
+  }
+  if (event.target.classList.contains("numbers__decimal")) {
+    const decimal = event.target.innerText;
+    console.log("decimal ", decimal);
+    handleDecimal(decimal);
+  }
+
+  setDisplay();
+  console.log(calculator);
+};
 
 setDisplay();
 
 operatorButtons.addEventListener("click", handleClick);
 numberButtons.addEventListener("click", handleClick);
 clearButtons.addEventListener("click", handleClick);
-
-
-
-
-
